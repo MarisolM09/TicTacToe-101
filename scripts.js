9ce9bb4f69906c03a348ba2286f78de2f2e79e75
MarisolM09 <mmendez9980@gmail.com>
2022-03-07 19:06:38 -0600

    working on it

--- scripts.js
+++ scripts.js
@@ -27,16 +27,6 @@ const handleClick = (element) => {
   }
 }
 
-
-
-
-
-
-
-
-
-
-
 // this function places the "currentMarker" inside the HTML element that was clicked and calls the "changeMarker" function.
 const addMarker = (id) => {
 
@@ -46,18 +36,8 @@ const addMarker = (id) => {
   
 
   let element = document.getElementById(id).innerHTML = currentMarker;
-
-  changeMarker()
+  checkForWin()
 }
-
-
-
-
-
-
-
-
-
 
 // This "changeMarker" function changes "X" to "O" in the "currentMarker" variable or "O" to "X"
 const changeMarker = () => {
@@ -67,14 +47,6 @@ const changeMarker = () => {
     currentMarker = "X"
   }
 }
-
-
-
-
-
-
-
-
 
 
 // This "resetBoard" function is called when the user clicks on the "Restart" button.
@@ -110,19 +82,25 @@ const checkForWin = () => {
 }
 
 const horizontalWin = () => {
-  if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") 
-  || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")
-)
+  if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X" && board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X" && board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") 
+  || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O" && board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O" && board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O")
+) { return true
+
+} else { return false}
 }
 
 const verticalWin = () => {
   if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
   || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")
-)
+) { return true
+
+} else { return false}
 }
 
 const diagonalWin = () => {
   if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") 
   || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")
-)
+) { return true
+
+} else { return false}
 }
