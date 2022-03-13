fa9239e10517b6ddc7924219c0eaced914cf4043
MarisolM09 <mmendez9980@gmail.com>
2022-03-13 12:10:51 -0500

    added

--- scripts.js
+++ scripts.js
@@ -82,25 +82,38 @@ const checkForWin = () => {
 }
 
 const horizontalWin = () => {
-  if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X" && board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X" && board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") 
-  || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O" && board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O" && board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O")
-) { return true
+  if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") 
+  || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")
 
-} else { return false}
+  || (board[1][0] == "X" && board[1][1] == "O" && board[1][2] == "O")
+  || (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O")
+
+  || (board[2][0] == "X" && board[2][1] == "O" && board[2][2] == "O")
+  || (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O")) 
+  { return true
+} 
 }
 
 const verticalWin = () => {
   if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
   || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")
-) { return true
+  
+  || (board[1][0] == "X" && board[1][1] == "O" && board[1][2] == "O")
+  || (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O")
 
-} else { return false}
+  || (board[2][0] == "X" && board[2][1] == "O" && board[2][2] == "O")
+  || (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O"))
+  { return true
+} 
 }
 
 const diagonalWin = () => {
   if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") 
   || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")
-) { return true
 
-} else { return false}
+  || (board[0][2] == "X" && board[1][1] == "O" && board[2][1] == "O")
+  || (board[0][2] == "O" && board[1][1] == "O" && board[2][1] == "O"))
+  
+  { return true
+}
 }
